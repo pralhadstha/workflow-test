90918442ee1c5095903c6049fb6be6b2fda1ec05
Pralhad Kumar Shrestha <pralhad.shrestha05@gmail.com>
2024-07-29 12:15:05 +0545

    Code for testing autoformating.

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+
+
+
+
+    /**
+     * Display a listing of the resource.
+     * @param \App\Models\User $user
+     * @param  string $gets
+     * @return array
+     */
+    public function index($user, $gets)
+    {
+        //
+    }
+
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Test $test)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Test $test)
+    {
+        //
+    }
+}
---
+++ app/Models/Test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+                use HasFactory;
+
+            /**
+     * The attributes that are mass assignable.
+            *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+            'email',
+        'password',
+    ];
+
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+            *
+            * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
